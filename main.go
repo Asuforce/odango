2f64952606e9127874bd772a98f24ebcfa456d25
asuforce <owata.sn@gmail.com>
2019-07-19 17:57:21 +0900

    Handle parameter

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello!\n")
+func deployHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Method not allowed.\n")
+	}
+
+	s := r.URL.Path[len("/deploy/"):]
+
+	fmt.Fprintf(w, "Env is %s !\n", s)
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/deploy/", deployHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
