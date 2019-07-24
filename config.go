edc5ced14f658707b29ff8bd1bf3d058115d0e15
asuforce <owata.sn@gmail.com>
2019-07-24 17:29:01 +0900

    Set default setting file

--- config.go
+++ config.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/go-homedir"
 )
 
 type gongchaConfig struct {
@@ -45,8 +50,51 @@ type deployConfig struct {
 }
 
 func readConfig(config gongchaConfig) gongchaConfig {
-	if _, err := toml.DecodeFile("credential.toml", &config); err != nil {
+	home, _ := homedir.Dir()
+	checkConfigFile(home)
+	if _, err := toml.DecodeFile(home +"/.credential.toml", &config); err != nil {
 		exitErrorf("Unable to credential file, %v", err)
+		os.Exit(1)
 	}
 	return config
+}
+
+func checkConfigFile(home string) {
+	f, err := os.OpenFile(home +"/.credential.toml", os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	config := `
+[server]
+endpoint = "deploy" # Optional
+port = 8080 # Optional
+
+[credential]
+access_key = ""
+secret_key = ""
+endpoint = ""
+region = ""
+disable_ssl = false
+s3_force_path_style = true
+
+[bucket]
+name = ""
+path = ""
+extension = ""
+
+[ssh]
+user_name = ""
+key_path = ""
+hosts = ["", ""]
+port = 22 # Optional
+
+[deploy]
+archive_dir = ""
+dest_dir = ""
+
+`
+
+	fmt.Fprintln(f, config)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/aws/aws-sdk-go v1.21.2
 	github.com/bramvdbogaerde/go-scp v0.0.0-20190531185457-8e071620938c
+	github.com/mitchellh/go-homedir v1.1.0
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
 )
