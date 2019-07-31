7b30f287353f74fecb1750fe7ca22e89d0c56944
asuforce <owata.sn@gmail.com>
2019-07-31 14:43:58 +0900

    Style

--- config.go
+++ config.go
@@ -9,47 +9,49 @@ import (
 	"github.com/mitchellh/go-homedir"
 )
 
-// Config is odango configuration structure
-type Config struct {
-	home       string
-	Server     serverConfig
-	Credential credentialConfig
-	Bucket     bucketConfig
-	SSH        sshConfig `toml:"ssh"`
-	Deploy     deployConfig
-}
+type (
+	// Config is odango configuration structure
+	Config struct {
+		home       string
+		Server     serverConfig
+		Credential credentialConfig
+		Bucket     bucketConfig
+		SSH        sshConfig `toml:"ssh"`
+		Deploy     deployConfig
+	}
 
-type serverConfig struct {
-	Endpoint string
-	Port     int
-}
+	serverConfig struct {
+		Endpoint string
+		Port     int
+	}
 
-type credentialConfig struct {
-	AccessKey        string `toml:"access_key"`
-	SecretKey        string `toml:"secret_key"`
-	Endpoint         string
-	Region           string
-	DisableSSL       bool `toml:"disable_ssl"`
-	S3ForcePathStyle bool `toml:"s3_force_path_style"`
-}
+	credentialConfig struct {
+		AccessKey        string `toml:"access_key"`
+		SecretKey        string `toml:"secret_key"`
+		Endpoint         string
+		Region           string
+		DisableSSL       bool `toml:"disable_ssl"`
+		S3ForcePathStyle bool `toml:"s3_force_path_style"`
+	}
 
-type bucketConfig struct {
-	Name      string
-	Path      string
-	Extension string
-}
+	bucketConfig struct {
+		Name      string
+		Path      string
+		Extension string
+	}
 
-type sshConfig struct {
-	UserName string `toml:"user_name"`
-	KeyPath  string `toml:"key_path"`
-	Hosts    []string
-	Port     int
-}
+	sshConfig struct {
+		UserName string `toml:"user_name"`
+		KeyPath  string `toml:"key_path"`
+		Hosts    []string
+		Port     int
+	}
 
-type deployConfig struct {
-	ArchiveDir string `toml:"archive_dir"`
-	DestDir    string `toml:"dest_dir"`
-}
+	deployConfig struct {
+		ArchiveDir string `toml:"archive_dir"`
+		DestDir    string `toml:"dest_dir"`
+	}
+)
 
 func (c *Config) readConfig() {
 	c.home, _ = homedir.Dir()
